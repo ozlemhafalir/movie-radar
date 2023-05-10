f7de2d7887f349ec24683ebc101069c6f654521c
Özlem Hafalır <ozlemoztep@gmail.com>
2023-05-10 04:46:01 +0200

    Install cinemagoer library, get top imdb movies and randomize

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+from imdb import IMDbError, Cinemagoer
+
+NUMBER_OF_MOVIES = 30
+cinemagoer = Cinemagoer()
+
+
+def get_randomized_top_250_movies():
+    """
+    Returns a random list of movies from top 250 IMDB movies.
+    """
+    try:
+        movies = cinemagoer.get_top250_movies()
+        random_indexes = random.sample(range(0, len(movies)), NUMBER_OF_MOVIES)
+        return [movies[index] for index in random_indexes]
+    except IMDbError:
+        print("Error during getting movies from Cinemagoer")
+        return []
+
+
+def main():
+    """Starts the game."""
+    random_movies = get_randomized_top_250_movies()
+    print(random_movies)
+
+
+main()
